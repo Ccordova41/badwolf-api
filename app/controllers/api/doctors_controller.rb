58c1eb18515fd4b792e810dfc03d61ccfd9b16ff
Ccordova41 <ccordova41@gmail.com>
2018-02-21 02:30:06 -0500

    add doctor routes

--- app/controllers/api/doctors_controller.rb
+++ app/controllers/api/doctors_controller.rb
@@ -1,2 +1,13 @@
 class DoctorsController < ApplicationController
+  def index
+    @resp = Faraday.get 'https://api.giphy.com/v1/gifs/search' do |req|
+      req.params['api_key'] = 'o5nM6wKjgx6DAHYh9Hmrkp46osWK7CSe'
+      req.params['q'] = 'doctorwho'
+      req.params['limit'] = 100
+    end
+    body_hash = JSON.parse(@resp.body)
+    @doctors = body_hash["data"].sample(15)
+    render json: @doctors
+  end
+
 end
