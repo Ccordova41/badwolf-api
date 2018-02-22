ea5e1180c75fa682273588156fa861e237f0d3de
Ccordova41 <ccordova41@gmail.com>
2018-02-21 19:49:26 -0500

    added reports controller and edited actions

---
+++ app/controllers/api/reports_controller.rb
@@ -0,0 +1,32 @@
+class Api::ReportsController < ApplicationController
+
+  def index
+    @reports = Report.all
+    render json: @reports
+  end
+
+  def create
+    @report = Report.new(report_params)
+    if @report.save
+      render json: @report
+    else
+      render json: { errors: { message: "FAILED TO POST REPORT"}}
+    end
+  end
+
+  def update
+    @report = Report.find_by(id: params[:id])
+    if @report.update(report_params)
+      render json: @report
+    else
+      render json: { errors: { message: "FAILED TO POST REPORT"}}
+    end
+  end
+
+private
+
+  def report_params
+    params.require(:report).permit(:name, :doctor_id)
+  end
+
+end
